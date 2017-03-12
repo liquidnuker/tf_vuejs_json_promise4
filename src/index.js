8b4033537a97c1b2f8a491eb45d4046f291abd45
administrator <liquidnuker@users.noreply.github.com>
2017-03-11 16:12:35 -0800

    separated modules

--- src/index.js
+++ src/index.js
@@ -2,30 +2,12 @@ import "./styles/main.scss";
 
 import {store, vmA, vmB, vmC} from "./js/vue-components";
 import {jsonLoader} from './js/jsonLoader.js';
-import "./js/vendor/jPages.min.js";
 import {jsonFilter} from './js/jsonFilter.js';
+import {showPages} from "./js/paginator.js";
 
 // 
 // ======================================================/
 const jsonUrl = "src/js/ajax/bonsai.json";
-
-// 
-// ======================================================/
-const showPages = () => {
-  $("#paginator").jPages({
-    containerID: "galleryContainer",
-    first: "first",
-    previous: "previous",
-    next: "next",
-    last: "last",
-    links: "numeric", // blank || title
-    delay: 0, // to remove fade
-    fallback: 0, // to remove fade
-    startPage: 1,
-    perPage: 10,
-    midRange: 5,
-  });
-};
 
 jsonLoader.preloader();
 jsonLoader.getJSON(jsonUrl).then(function (response) {
